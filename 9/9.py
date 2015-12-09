81491a31a47db99a4a54c7679c09f73b9494d50a
Christian Kullander <christian@kullander.nu>
2015-12-09 20:22:26 +0100

    A little tweaking and shrinking (zip is fun!)

--- 9/9.py
+++ 9/9.py
@@ -3,16 +3,6 @@
 import re
 from itertools import permutations
 
-def calc_distance(path_tuple, distances):
-    path = list(path_tuple)
-    cost = 0
-    a = path.pop(0)
-    while len(path) > 0:
-        b = path.pop(0)
-        cost += distances[(a, b)]
-        a = b
-    return cost
-
 def do(input_file):
     with open(input_file) as f:
         strings = [s.strip() for s in f.readlines()]
@@ -21,27 +11,19 @@ def do(input_file):
     routes = dict()
     for s in strings:
         origin, destination, distance = re.match(r"(\w+) to (\w+) = (\d+)", s).groups()
-        places.add(origin)
-        places.add(destination)
+        places.update([origin, destination])
         distances[(origin, destination)] = int(distance)
         distances[(destination, origin)] = int(distance)
     # for larger problem sets -> build graph and use Dijkstra
-    place_count = len(places)
-    min_distance = 10000000
+    min_distance, max_distance = 10000000, 0
     max_distance = 0
-    min_path = ("N/A")
-    max_path = ("N/A")
     for p in permutations(places):
-       if len(p) == place_count:
-           this_distance = calc_distance(p, distances)
-           if this_distance < min_distance:
-               min_distance = this_distance
-               min_path = p
-           if this_distance > max_distance:
-               max_distance = this_distance
-               max_path = p
-    print "shortest", min_path, min_distance
-    print "longest", max_path, max_distance
+       if len(p) == len(places):
+           this_distance = sum([distances[od] for od in zip(p, p[1:])])
+           min_distance = min(min_distance, this_distance)
+           max_distance = max(max_distance, this_distance)
+    print "shortest", min_distance
+    print "longest", max_distance
 
 do('input-test')
 do('input')
