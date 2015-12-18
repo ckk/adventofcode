9e860dac410cf7cade35941683f7cdecad2c1f8b
Christian Kullander <christian@kullander.nu>
2015-12-18 22:05:19 +0100

    Day 18, slow initial version

---
+++ 18/18.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from copy import deepcopy
+from itertools import ifilter, product, repeat
+
+def neighbors(grid, x, y):
+    result = list()
+    for nx in range(max(0, x-1), min(x+2, len(grid[0]))):
+        for ny in range(max(0, y-1), min(y+2, len(grid))):
+            if not (nx == x and ny == y):
+                result.append(grid[ny][nx])
+    return result
+
+def life(input_file, generations, stuck=False):
+    with open(input_file) as f:
+        grid = [[True if c == '#' else False for c in row.strip()] for row in f.readlines()]
+    if stuck:
+        grid[0][0], grid[0][-1],  grid[-1][0], grid[-1][-1] = True, True, True, True
+    for g in range(generations):
+        newgrid = deepcopy(grid) 
+        for x in range(len(grid[0])):
+            for y in range(len(grid)):
+                newgrid[y][x] = (grid[y][x] and 2 <= neighbors(grid, x, y).count(True) <= 3) or (not grid[y][x] and neighbors(grid, x, y).count(True) == 3)
+        grid = newgrid
+        if stuck:
+            grid[0][0], grid[0][-1],  grid[-1][0], grid[-1][-1] = True, True, True, True
+    print("lights: %d" % sum([row.count(True) for row in grid]))
+                
+life('input-test', 4)
+life('input-test', 5, stuck=True)
+life('input', 100)
+life('input', 100, stuck=True)
